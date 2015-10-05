11a967571b70eeaff101f9c27e659f619eb5cfaa
Hector Dearman <hector.dearman@gmail.com>
2015-10-05 17:18:53 +0100

    add url to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='hector.dearman@gmail.com',
     packages=['essence'],
     scripts=[],
-    url='',
+    url='https://github.com/chromy/essence.git',
     license='LICENSE.txt',
     description='',
     long_description=open('README.txt').read(),
