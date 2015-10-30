e0a7e6b9ff53cfeb06286210495cbae85a1e782b
Hector Dearman <hector.dearman@gmail.com>
2015-10-30 23:02:55 +0000

    remove test command and move module to under src

--- setup.py
+++ setup.py
@@ -1,47 +1,14 @@
 import sys
-from subprocess import call
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
-
-class EssenceTest(TestCommand):
-    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.tox_args = None
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tox(self):
-        import tox
-        import shlex
-        args = self.tox_args
-        if args:
-            args = shlex.split(self.tox_args)
-        errno = tox.cmdline(args=args)
-        return errno
-
-    def run_check_manifest(self):
-        return call(['check-manifest'])
-
-    def run_tests(self):
-        errno = self.run_check_manifest()
-        if errno != 0:
-            return sys.exit(errno)
-        errno = self.run_tox()
-        if errno != 0:
-            return sys.exit(errno)
-
 
 setup(
     name='essence',
     version='0.0.1',
     author='Hector Dearman',
     author_email='hector.dearman@gmail.com',
-    packages=['essence'],
+    packages=find_packages('src'),
+    package_dir={'':'src'},
     scripts=[],
     url='https://github.com/chromy/essence.git',
     license='MIT',
@@ -58,10 +25,6 @@ setup(
         'tox',
         'check-manifest>=0.25',
     ],
-
-    cmdclass = {
-        'test': EssenceTest
-    },
 
     classifiers=[
         # Status
